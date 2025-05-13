b0fe571551832cb94e2295f7edc0a43faa9de030
Mirza Ali Jaffer <mirzaalijaffer@gmail.com>
2025-05-13 15:32:46 +0530

    Completed the required task, by adding images upload, archive and download

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+# External libraries
 import streamlit as st
 import numpy as np
 import cv2
+
+#Python modules
 import warnings
 warnings.filterwarnings('ignore')
+from zipfile import ZipFile
+import zlib     # for compression="zipfile.ZIP_DEFLATED"
+import shutil
+import gc       # for garbage collection
+gc.enable()       
+import os
 
 
 st.title("Augmented Image Generator")
+
 
 # Transformation Functions:
 
@@ -67,55 +77,94 @@ def Cropping(img,n=5):
         return cr_img
         # save_image_and_show(img_crop)
 
-img = cv2.imread("/Users/ali/Desktop/data_science/ML/computer_vision/Apple-logo-1977.jpg")
 
-st.file_uploader("Upload image",type="zip",accept_multiple_files=True)
+# Function for combining and applying transformations on a single image
+def combined_trans(trans_types,img):
+    trans_img = img
+    for i in trans_types:
+        trans_img = eval(f"{i}(trans_img)")
+    return trans_img
+
+
+# Taking Files from the User
+# img = cv2.imread("/Users/ali/Desktop/data_science/ML/computer_vision/Apple-logo-1977.jpg")    #example image
+files = st.file_uploader("Upload image",type=["jpg","png","zip"],accept_multiple_files=True)    
+# Future Imporvements: Add functionality to accept compressed zip files and then extract them.
+
+# Reading/Converting files to cv2.image()/array 
+images = []
+for i,file in enumerate(files):
+
+    # 1) converting to a 1D uint8 NumPy array
+    np_arr = np.frombuffer(file.read(), dtype=np.uint8)     #file.read() is to read bytes from the UploadedFile
+
+    # 2) decoding into an OpenCV image (BGR color by default)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    # Appending image arrays to images list
+    images.append(img)
+
+    files[i] = None     # Clearing files simultaneous to the conversion
+
+# Deleting files from RAM
+del files
+# ;del file;del img;del np_arr  
+
 
 
 with st.form(key='transformation'):
 
-    trans = st.pills("**Select transformations**",options=["Translation","Rotation","Shearing","Cropping","Scaling"],selection_mode="multi")
+    trans_types = st.pills("**Select transformations**",options=["Translation","Rotation","Shearing","Cropping","Scaling"],selection_mode="multi")
 
     preview = st.form_submit_button("Preview")
-    if preview: 
-
-        def combined_trans(selected):
-            trans_img = img
-            for i in selected:
-                trans_img = eval(f"{i}(trans_img)")
-            return trans_img
-
-        
-        st.image(combined_trans(trans))
+    if preview:# convert to rgb because streamlit supports rgb
+        st.image(combined_trans(trans_types,images[0]))  #files[0] is the preview image
 
 # with st.container():
-
 #     trans = st.pills("Select transformations",options=["Translation","Rotation","Shearing","Cropping","Scaling"],selection_mode="multi")
-
 #     def combined_trans(selected):
 #         trans_img = img
 #         for i in selected:
 #             trans_img = eval(f"{i}(trans_img)")
-#         return trans_img
-
-        
+#         return trans_img        
 #     st.image(combined_trans(trans))
 
 
-if len(trans)>0:
+if len(trans_types)>0:
 
     with st.popover("Click to view selected transformations"):
-        # st.write("**Selected transformations**",)
-        for n,i in enumerate(trans,start=1):
-            st.write(n,i)
+        for n,i in enumerate(trans_types,start=1):
+            st.write(f"{n}.{i}")
         st.write("(Press 'Preview' to confirm change in selection)")
 
-    data = 0
+    
     with st.form(key='download'):
         img_count = st.slider("Select number of images to download",1,50)
-        confirm = st.form_submit_button("Confirm")
+        # compress_level = st.number_input("Enter compression level",0,8)
 
+        confirm = st.form_submit_button("Confirm")
+        
     if confirm:
-        st.download_button("Download",data,"data.csv")
-      
+
+        if not os.path.exists("data/augmented_images"):
+            os.mkdir("data/augmented_images")
+        # Creating zip file, and writing images files to it one by one
+        # with ZipFile("data/augmented_images.zip","w",compression=8, compresslevel=compress_level) as zip:   #compression=8 means zipfile.ZIP_DEFLATED 
+        for i,img in enumerate(images):
+            for n in range(img_count):
+                cv2.imwrite(f"data/augmented_images/{i}{n}.jpg",combined_trans(trans_types,img))
+
+        shutil.make_archive("data/augmented_images","zip",base_dir="data/augmented_images")
+
+        with open("data/augmented_images.zip",'rb') as f:
+            download_button = st.download_button("Download images",f,file_name="augmented_images.zip")
+
+            # if download_button:     
+                # if os.path.exists("data/augmented_images.zip") and os.path.exists("data/augmented_images"):
+        # Clearing the created folder and files from data directory         
+        os.remove("data/augmented_images.zip")
+        shutil.rmtree("data/augmented_images")
+
+                # os.rmdir("data/augmented_images.zip")      #Deleting the files from dir after user download completes
+
 
